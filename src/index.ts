e564a73303cddd0a58ca7d8b458e34dcd2d0c1b4
Tarasov-Front-Dev <tarasov.front.dev@gmail.com>
2024-05-24 18:17:31 -0600

    refactor: code

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import './style.css'
 
-export { sum } from './sum'
 export * from './algorithms'
 export * from './interviewTasks'
+export * from './sberIt'
+export * from './search_algs'
+export * from './sorting_algs'
